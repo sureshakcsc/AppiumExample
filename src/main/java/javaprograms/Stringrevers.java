8ba6dd26c1fc301efac472faf3b909c4ecdcccef
DELL <DELL@192.168.1.6>
2021-08-05 10:44:59 +0530

    Added javaprograms to branchx

---
+++ src/main/java/javaprograms/Stringrevers.java
@@ -0,0 +1,15 @@
+package javaprograms;
+
+public class Stringrevers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String  s1="suresh";
+		StringBuffer s= new  StringBuffer();
+		s.append(s1);
+		System.out.println(s.reverse());
+
+	}
+
+}
